5f84e32c0e12eeea39cc1e4fd4e0a77f20381330
Filip2332 <fikiroki00@gmail.com>
2025-03-23 14:34:43 +0100

    added cities model and made a table(fixed)

--- database/migrations/2025_03_23_133123_create_cities_models_table.php
+++ database/migrations/2025_03_23_133123_create_cities_models_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('weather', function (Blueprint $table) {
-            $table->dropColumn('city');
-
-
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
         });
     }
 
@@ -23,8 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('weather', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('cities');
     }
 };
