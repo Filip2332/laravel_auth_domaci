7f68ba40864a5be1db3679dbf952d974b3dccf59
Filip2332 <fikiroki00@gmail.com>
2025-03-23 14:14:25 +0100

    added cities model and made a table

---
+++ database/seeders/CreateCitiesSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CreateCitiesSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        Cities::create([
+
+        ]);
+    }
+}
